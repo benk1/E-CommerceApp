598bcd4893043ff723d5d685c2a17dac5df4239c
benk1 <kakengi_bernard@hotmail.com>
2019-12-15 04:09:21 +0200

    update Menu-item component with High order component

--- src/components/Directory.jsx
+++ src/components/Directory.jsx
@@ -9,29 +9,34 @@ class Directory extends Component {
         title: 'hats',
         imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
 
-        id: 1
+        id: 1,
+        linkUrl: 'hats'
       },
       {
         title: 'jackets',
         imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-        id: 2
+        id: 2,
+        linkUrl: ''
       },
       {
         title: 'sneakers',
         imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-        id: 3
+        id: 3,
+        linkUrl: ''
       },
       {
         title: 'womens',
         imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
         size: 'large',
-        id: 4
+        id: 4,
+        linkUrl: ''
       },
       {
         title: 'mens',
         imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         size: 'large',
-        id: 5
+        id: 5,
+        linkUrl: ''
       }
     ]
   };
@@ -43,6 +48,7 @@ class Directory extends Component {
         {sections.map(section => (
           <MenuItem
             key={section.id}
+            linkUrl={section.linkUrl}
             title={section.title}
             imageUrl={section.imageUrl}
             size={section.size}
